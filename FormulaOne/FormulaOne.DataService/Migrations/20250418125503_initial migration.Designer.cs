3519818631f9c7588a47816e86fc6be760548c15
GeorgeHarbor <ldjordjevic@protonmail.com>
2025-04-18 16:45:50 +0200

    Add db and migrations

--- FormulaOne/FormulaOne.DataService/Migrations/20250418125503_initial migration.Designer.cs
+++ FormulaOne/FormulaOne.DataService/Migrations/20250418125503_initial migration.Designer.cs
@@ -10,104 +10,104 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FormulaOne.DataService.Migrations
 {
-    [DbContext(typeof(AppDbContext))]
-    [Migration("20250418125503_initial migration")]
-    partial class initialmigration
+  [DbContext(typeof(AppDbContext))]
+  [Migration("20250418125503_initial migration")]
+  partial class InitialMigration
+  {
+    /// <inheritdoc />
+    protected override void BuildTargetModel(ModelBuilder modelBuilder)
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
-        {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "8.0.15");
+      modelBuilder.HasAnnotation("ProductVersion", "8.0.15");
 
-            modelBuilder.Entity("FormulaOne.Entities.DbSet.Achievement", b =>
-                {
-                    b.Property<Guid>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("TEXT");
+      modelBuilder.Entity("FormulaOne.Entities.DbSet.Achievement", b =>
+          {
+            b.Property<Guid>("Id")
+                      .ValueGeneratedOnAdd()
+                      .HasColumnType("TEXT");
 
-                    b.Property<DateTime>("AddedDate")
-                        .HasColumnType("TEXT");
+            b.Property<DateTime>("AddedDate")
+                      .HasColumnType("TEXT");
 
-                    b.Property<Guid>("Driverid")
-                        .HasColumnType("TEXT");
+            b.Property<Guid>("Driverid")
+                      .HasColumnType("TEXT");
 
-                    b.Property<int>("FastestLap")
-                        .HasColumnType("INTEGER");
+            b.Property<int>("FastestLap")
+                      .HasColumnType("INTEGER");
 
-                    b.Property<int>("PolePosition")
-                        .HasColumnType("INTEGER");
+            b.Property<int>("PolePosition")
+                      .HasColumnType("INTEGER");
 
-                    b.Property<int>("RaceWins")
-                        .HasColumnType("INTEGER");
+            b.Property<int>("RaceWins")
+                      .HasColumnType("INTEGER");
 
-                    b.Property<int>("Status")
-                        .HasColumnType("INTEGER");
+            b.Property<int>("Status")
+                      .HasColumnType("INTEGER");
 
-                    b.Property<DateTime>("UpdatedDate")
-                        .HasColumnType("TEXT");
+            b.Property<DateTime>("UpdatedDate")
+                      .HasColumnType("TEXT");
 
-                    b.Property<int>("WorrldChampionship")
-                        .HasColumnType("INTEGER");
+            b.Property<int>("WorrldChampionship")
+                      .HasColumnType("INTEGER");
 
-                    b.HasKey("Id");
+            b.HasKey("Id");
 
-                    b.HasIndex("Driverid");
+            b.HasIndex("Driverid");
 
-                    b.ToTable("Achievements");
-                });
+            b.ToTable("Achievements");
+          });
 
-            modelBuilder.Entity("FormulaOne.Entities.DbSet.Driver", b =>
-                {
-                    b.Property<Guid>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("TEXT");
+      modelBuilder.Entity("FormulaOne.Entities.DbSet.Driver", b =>
+          {
+            b.Property<Guid>("Id")
+                      .ValueGeneratedOnAdd()
+                      .HasColumnType("TEXT");
 
-                    b.Property<DateTime>("AddedDate")
-                        .HasColumnType("TEXT");
+            b.Property<DateTime>("AddedDate")
+                      .HasColumnType("TEXT");
 
-                    b.Property<DateTime>("DateOfBirth")
-                        .HasColumnType("TEXT");
+            b.Property<DateTime>("DateOfBirth")
+                      .HasColumnType("TEXT");
 
-                    b.Property<int>("DriversNumber")
-                        .HasColumnType("INTEGER");
+            b.Property<int>("DriversNumber")
+                      .HasColumnType("INTEGER");
 
-                    b.Property<string>("FirstName")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+            b.Property<string>("FirstName")
+                      .IsRequired()
+                      .HasColumnType("TEXT");
 
-                    b.Property<string>("LastName")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
+            b.Property<string>("LastName")
+                      .IsRequired()
+                      .HasColumnType("TEXT");
 
-                    b.Property<int>("Status")
-                        .HasColumnType("INTEGER");
+            b.Property<int>("Status")
+                      .HasColumnType("INTEGER");
 
-                    b.Property<DateTime>("UpdatedDate")
-                        .HasColumnType("TEXT");
+            b.Property<DateTime>("UpdatedDate")
+                      .HasColumnType("TEXT");
 
-                    b.HasKey("Id");
+            b.HasKey("Id");
 
-                    b.ToTable("Drivers");
-                });
+            b.ToTable("Drivers");
+          });
 
-            modelBuilder.Entity("FormulaOne.Entities.DbSet.Achievement", b =>
-                {
-                    b.HasOne("FormulaOne.Entities.DbSet.Driver", "Driver")
-                        .WithMany("Achievements")
-                        .HasForeignKey("Driverid")
-                        .OnDelete(DeleteBehavior.NoAction)
-                        .IsRequired()
-                        .HasConstraintName("FK_Achievements_Driver");
+      modelBuilder.Entity("FormulaOne.Entities.DbSet.Achievement", b =>
+          {
+            b.HasOne("FormulaOne.Entities.DbSet.Driver", "Driver")
+                      .WithMany("Achievements")
+                      .HasForeignKey("Driverid")
+                      .OnDelete(DeleteBehavior.NoAction)
+                      .IsRequired()
+                      .HasConstraintName("FK_Achievements_Driver");
 
-                    b.Navigation("Driver");
-                });
+            b.Navigation("Driver");
+          });
 
-            modelBuilder.Entity("FormulaOne.Entities.DbSet.Driver", b =>
-                {
-                    b.Navigation("Achievements");
-                });
+      modelBuilder.Entity("FormulaOne.Entities.DbSet.Driver", b =>
+          {
+            b.Navigation("Achievements");
+          });
 #pragma warning restore 612, 618
-        }
     }
+  }
 }
