d0d94d69651d610b22192655da6cbba16c58784a
GeorgeHarbor <ldjordjevic@protonmail.com>
2025-04-18 16:59:03 +0200

    Fix typo in Achievements

---
+++ FormulaOne/FormulaOne.DataService/Migrations/20250418145532_FixTypo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FormulaOne.DataService.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTypo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "WorrldChampionship",
+                table: "Achievements",
+                newName: "WorldChampionship");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "WorldChampionship",
+                table: "Achievements",
+                newName: "WorrldChampionship");
+        }
+    }
+}
--- FormulaOne/FormulaOne.Entities/DbSet/Achievement.cs
+++ FormulaOne/FormulaOne.Entities/DbSet/Achievement.cs
@@ -5,7 +5,7 @@ public class Achievement : BaseEntity
   public int RaceWins { get; set; }
   public int PolePosition { get; set; }
   public int FastestLap { get; set; }
-  public int WorrldChampionship { get; set; }
+  public int WorldChampionship { get; set; }
   public Guid Driverid { get; set; }
 
   public virtual Driver? Driver { get; set; }
